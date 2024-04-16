c92456570867022e61fe3d596464baac950ba682
jessienguyen2312 <jessie.nguyen96@gmail.com>
2024-04-16 17:08:51 -0400

    connect mongodb to server

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
 import express from 'express';
+import mongoose from "mongoose";
+import session from "express-session";
+import cors from "cors";
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING, {dbName: "bookazon"});
+
 const app = express();
 app.listen(4000);
